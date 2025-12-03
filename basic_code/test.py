d08c3820e208452af674b47f0b559b42e2661e9c
jmh <2388575287@qq.com>
2025-12-03 20:44:41 +0800

    add  y3

--- basic_code/test.py
+++ basic_code/test.py
@@ -8,13 +8,18 @@ print(a)
 x = np.arange(1,10,0.1)
 y1 = np.sin(x)
 y2 = np.cos(x)
+y3 = np.sin(x**2)
 
 #绘制图形
-plt.plot(x, y1, label="sin")
-plt.plot(x, y2, linestyle="--", label="sin")
+plt.plot(x, y1, label="sin(x)")
+plt.plot(x, y2, linestyle=":", label="cos(x)")
+plt.plot(x, y3, linestyle="--", label="sin(x²)")
+plt.title("sin&cos")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("sin&cos")
 plt.legend()
 plt.show()
+
+
 
