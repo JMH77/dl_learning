bb469aa9fbd2eedf4a8ddf8de835b6eb33961925
jmh <2388575287@qq.com>
2025-12-03 20:27:56 +0800

    try to use  matplotlib

--- basic_code/test.py
+++ basic_code/test.py
@@ -1,5 +1,20 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 print("test")
 a = np.array([1,2,3])
 print(a)
+
+x = np.arange(1,10,0.1)
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+#绘制图形
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label="sin")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("sin&cos")
+plt.legend()
+plt.show()
+
